89199ed8f0f9b047044b4ab13ab89e5c39a49532
Seita <yasuta@example.com>
2025-08-26 09:22:16 +0900

    change

--- reddit_api/api.py
+++ reddit_api/api.py
@@ -1,15 +1,4 @@
 import praw
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-client_id = os.environ.get("client_id")
-client_secret = os.environ.get("client_secret")
-user_agent = os.environ.get("user_agent")
-
-reddit = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,
-    user_agent=user_agent,
-)
+reddit = praw.Reddit("bot1")
 print(reddit)
